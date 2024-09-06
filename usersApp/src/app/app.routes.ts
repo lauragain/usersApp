ef3680500d10a1393ebc35b955ce9d21a519e17b
Laura <laugain8@gmail.com>
2024-09-06 19:49:00 +0200

    Creación de las rutas

--- usersApp/src/app/app.routes.ts
+++ usersApp/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch: 'full', redirectTo: 'home'},
+    {path: "home", component: HomeComponent},
+    {path: "dashboard", component: DashboardComponent, children: [
+        {path: "", pathMatch: 'full', redirectTo: 'users'},
+        {path: "users", component: UserListComponent},
+    ]},
+    
+    {path: '**', redirectTo: 'home'}
+];

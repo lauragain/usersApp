53ec322160c216700b4fbcd136995ea5a968f128
Laura <laugain8@gmail.com>
2024-09-06 18:58:25 +0200

    Servicio users: modificar configuración, inyectar 'HttpClient' y crear la `baseUrl` y getAll()

--- usersApp/src/app/services/users.service.ts
+++ usersApp/src/app/services/users.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUser } from '../interfaces/iuser.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private baseUrl: string = "https://peticiones.online/users"
+  
+  private http = inject(HttpClient)
 
-  constructor() { }
+  async getAll(): Promise<IUser[]>{
+    const page1 = await firstValueFrom(this.http.get<{ results: IUser[]}>(`${this.baseUrl}?page=1`))
+    const page2 = await firstValueFrom(this.http.get<{ results: IUser[]}>(`${this.baseUrl}?page=2`))
+    return [...page1.results, ...page2.results]
+  }
 }
